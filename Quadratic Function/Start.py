7559415fcf57ee1ac1b70426a6d3774a7ad723d9
Svein <sveinvindfjell@gmail.com>
2018-08-27 13:20:54 +0200

    Created Start.py

---
+++ Quadratic Function/Start.py
@@ -0,0 +1,9 @@
+print("Insert Quadratic Fuction")
+print("Example: ax^2+bx+c")
+
+#Collects the fuction from user. 
+strQuadratic = input("f(x)=")
+
+def quadraticSplitter(strToSplit):
+    
+    return splitResult
