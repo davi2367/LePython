954f7af7a78fe94d36e34d53a249f5b81470978d
unknown <david@voelker.dk>
2018-08-27 13:28:34 +0200

    dig

--- Quadratic Function/Start.py
+++ Quadratic Function/Start.py
@@ -6,5 +6,5 @@ strQuadratic = input("f(x)=")
 
 def quadraticSplitter(strToSplit):
     import re
-    match = re.search("EXPRESSION",strQuadratic)
+    match = re.search(r"\d{1,5}x\^2[+|-]\d{1,5}x[+|-]\d{1,5}",strQuadratic)
     return splitResult
